77cb4c075dcdb7f5bb58f5cfaa2248d30ad9ac44
Goutam Sachdeva <goutam.sach@gmail.com>
2023-09-25 22:12:18 +0530

    nbsp removed

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -24,7 +24,7 @@ function projects() {
           <div className="  flex items-center flex-col  justify-center ">
             {/* movix.................... */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;MUI,  &nbsp;API,&nbsp;React Player, &nbsp;React lazy load "
+              title="  html5,  css3,  JavaScript,  React JS,  Tailwind CSS,  Redux,  React Router, MUI,   API, React Player,  React lazy load "
               summary1="A feature-rich movie information website that shows the Trending, Popular, and Top Rated movies and TV shown in daily and weekly categories for the latest releases. You can get ratings, release dates, watch times, Casts, descriptions, Stream live trailers, Official videos, banners, and even much more."
               summary2="
               It also gives recommendations for similar movies you might be interested in. It is powered by TMDB API and deployed on vercel and is fully responsive.  "
@@ -35,7 +35,7 @@ function projects() {
             />
             {/* googleClone.................... */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;MUI, &nbsp;API "
+              title="  html5,  css3,  JavaScript,  React JS,  Tailwind CSS,  Redux,  React Router, MUI,  API "
               summary1="Google clone is a fully responsive demo web app that can deliver real-time search results taking input from users. It can also display images per user's input. It can navigate between pages and can deliver millions of results within milliseconds. "
               summary2="It is a realtime web app which is powered by Google's custom search API deployed on Netlify. "
               image={google_clone}
@@ -45,7 +45,7 @@ function projects() {
             />
             {/* airbnb_clone  */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS,&nbsp;Next JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;MUI, &nbsp;API "
+              title="  html5,  css3,  JavaScript,  React JS, Next JS,  Tailwind CSS,  Redux,  React Router, MUI,  API "
               summary1="Airbnb clone is a fully responsive  demo web app showcasing strong front-end skills. It is developed with NEXT and Tailwind. Search by choosing the preferred location for your stay and, the number of guest and make a selection for dates and it will deliver some predefined results to you. It is a cool app which deployed on Vercel. "
               image={Airbnb_clone}
               link="https://airbnb-deploy-eight.vercel.app/"
@@ -54,7 +54,7 @@ function projects() {
             />
             {/* Snapchat_clone */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;Firebase & Firestore,&nbsp;MUI, &nbsp;API "
+              title="  html5,  css3,  JavaScript,  React JS,  Tailwind CSS,  Redux,  React Router, Firebase & Firestore, MUI,  API "
               summary1="Snapchat Clone is a  demo web app showcasing strong front-end skills. Users can sign up using their Google account and can capture and share their moments. It uses a real-time database from Firebase to send and receive posts. It is a cool app that uses Firebase for hosting."
               image={snapchat_clone}
               link="https://snapchat-clone-3358c.web.app/"
@@ -63,7 +63,7 @@ function projects() {
             />
             {/* gmail_clone */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;Firebase & Firestore,&nbsp;MUI, &nbsp;API "
+              title="  html5,  css3,  JavaScript,  React JS,  Tailwind CSS,  Redux,  React Router, Firebase & Firestore, MUI,  API "
               summary1="Gmail clone is a demo web app showcasing strong front-end skills. It uses a real-time database from Firebase to send and receive emails. Users can Send and Read emails in no time. Users can sign up and start sending emails. It is embedded with a sign-in with a Google account option. It is a cool app which deployed on Firebase."
               image={gmail_clone}
               link="https://clone-5500d.web.app/"
@@ -72,7 +72,7 @@ function projects() {
             />
             {/* linkedIn_Clone */}
             <ProjectCard
-              title="&nbsp;&nbsp;html5,&nbsp; css3, &nbsp;JavaScript, &nbsp;React JS, &nbsp;Tailwind CSS, &nbsp;Redux, &nbsp;React Router,&nbsp;Firebase & Firestore,&nbsp;MUI, &nbsp;API "
+              title="  html5,  css3,  JavaScript,  React JS,  Tailwind CSS,  Redux,  React Router, Firebase & Firestore, MUI,  API "
               summary1="LinkedIn Clone is a  demo web app showcasing strong front-end skills. It is using a realtime database from firebase to manage its backend. Users can sign up and log in using email and password. Users can read feeds and the stuff others post on the app and can share their posts with others as well. Posts are updated on a realtime basis making it more interesting. It is deployed in firebase."
               image={Linkedin_clone}
               link="https://linkedin-clone-acae4.web.app/"
