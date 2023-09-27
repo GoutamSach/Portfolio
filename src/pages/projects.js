5baa7d7cf007e9e2b4e202539101f6697f9132bc
Goutam Sachdeva <goutam.sach@gmail.com>
2023-09-27 21:09:40 +0530

    try

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Animatedtext from "./components/Animatedtext";
-import project1 from "../../public/images/projects/movix.jpg";
-import google_clone from "../../public/images/projects/googleClone.jpg";
-import Airbnb_clone from "../../public/images/projects/Airbnb-Clone.jpg";
-import gmail_clone from "../../public/images/projects/gmail_clone.jpg";
-import Linkedin_clone from "../../public/images/projects/linkedin_clone.jpg";
-import snapchat_clone from "../../public/images/projects/snapchat_clone.jpg";
+import project1 from "../../public/images/projects/movix.JPG";
+import google_clone from "../../public/images/projects/googleClone.JPG";
+import Airbnb_clone from "../../public/images/projects/Airbnb-Clone.JPG";
+import gmail_clone from "../../public/images/projects/gmail_clone.JPG";
+import Linkedin_clone from "../../public/images/projects/linkedin_clone.JPG";
+import snapchat_clone from "../../public/images/projects/snapchat_clone.JPG";
 import TransitionEffects from "./components/TransitionEffects";
 import ProjectCard from "./components/ProjectCard";
 
